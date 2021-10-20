d07609191a7781a2edc7539ce8b1281c9a0bf55e
Leonce Lamien <leoncelamien@MacBook-Air-de-Leonce.local>
2021-10-20 18:22:52 +0200

    change-color

--- views/footer.php
+++ views/footer.php
@@ -8,7 +8,7 @@
 			<p id="copyright">Mise en page d'après
 				<a href="http://www.alsacreations.com/tutoriels/">Alsacréations</a>
 			</p>
-            <p>Sienou Lamien stepahne </p>
+            <p>Sienou Lamien stepahne leonce  </p>
 		</div><!-- #pied -->
 	</div><!-- #global -->
 	</body>
